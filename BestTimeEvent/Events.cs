230084a2a5f1338ea429ba6272620ebfa18be18e
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2024-11-01 19:06:11 +0300

    Etkinlik yerleri ile ilgili degiskenleri iceren model olusturuldu.

---
+++ BestTimeEvent/Events.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BestTimeEvent
+{
+    public class Events
+    {
+        public Events(int id, DateTime start_Time, DateTime end_Time, string location, int priority)
+        {
+            Id = id;
+            Start_Time = start_Time;
+            End_Time = end_Time;
+            Location = location;
+            Priority = priority;
+        }
+
+        public int Id { get; set; }
+        public DateTime Start_Time { get; set; }
+        public DateTime End_Time { get; set; }
+        public string Location { get; set; }
+        public int Priority { get; set; }
+    }
+}
+
+
