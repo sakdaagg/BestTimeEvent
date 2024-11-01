5badde16984a07e29cc914399309f6c699811762
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2024-11-01 19:07:17 +0300

    En iyi etkinlik secenegini hesaplayan method icin result model eklendi.

---
+++ BestTimeEvent/BestTimeEventResult.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace BestTimeEvent
+{
+    public class BestTimeEventResult
+    {
+        public BestTimeEventResult(List<int> bestEventIds, int total, int bestEventCount)
+        {
+            BestEventIds = bestEventIds;
+            Total = total;
+            BestEventCount = bestEventCount;
+        }
+
+        public List<int> BestEventIds { get; set; }
+        public int Total { get; set; }
+        public int BestEventCount { get; set; }
+    }
+}
