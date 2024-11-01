06d177b685277d5584e5c4ca2a04d6b53e8ea8a0
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2024-11-01 19:05:32 +0300

    Iki lokasyon arasi surelerin islenecegi matrix, modellestirildi.

---
+++ BestTimeEvent/Locations.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace BestTimeEvent
+{
+    public class Locations
+    {
+        public Locations(string from)
+        {
+            From = from;
+            Station_Minutes = new List<KeyValuePair<string, int>>();
+        }
+        public void AddTravelTime(string to, int time)
+        {
+            Station_Minutes.Add(new KeyValuePair<string, int>(to, time));
+        }
+
+        public int GetTravelTime(string to)
+        {
+            return Station_Minutes.Find(k => k.Key == to).Value;
+        }
+
+        public string From { get; set; }
+        public List<KeyValuePair<string,int>> Station_Minutes { get; set; }
+        
+    }
+}
